c07946de371462a0a4d5a2ac150e26677deb9faa
tuulingo <oliver.tuuling@tptlive.ee>
2022-10-20 15:09:37 +0300

    Initial commit

---
+++ src/main/java/com/ridango/game/model/Movie.java
@@ -0,0 +1,9 @@
+package com.ridango.game.model;
+
+import javax.persistence.Entity;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.sql.Date;
+
+

b998e48b08b0e0619e6e68dccefd65198ab361c5
tuulingo <oliver.tuuling@tptlive.ee>
2022-10-24 13:34:01 +0300

    Add mockito dependency

---
+++ src/test/java/com/ridango/game/service/MovieServiceTest.java
@@ -0,0 +1,15 @@
+package com.ridango.game.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class MovieServiceTest {
+
+    @Test
+    void findAllMovies() {
+    }
+
+    @Test
+    void findMovieById() {
+    }
+}

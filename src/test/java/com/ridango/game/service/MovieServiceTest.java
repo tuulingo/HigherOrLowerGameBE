8d271dca1be663252103625b65af9a031fe46c5e
tuulingo <oliver.tuuling@tptlive.ee>
2022-10-24 13:35:06 +0300

    Add canFindAllMovies test

--- src/test/java/com/ridango/game/service/MovieServiceTest.java
+++ src/test/java/com/ridango/game/service/MovieServiceTest.java
@@ -1,15 +1,37 @@
 package com.ridango.game.service;
 
+import com.ridango.game.model.Movie;
+import com.ridango.game.repository.MovieRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
 class MovieServiceTest {
 
-    @Test
-    void findAllMovies() {
+    @Mock
+    private MovieRepository movieRepository;
+    private MovieService underTest;
+
+    @BeforeEach
+    void setUp() {
+        underTest = new MovieService(movieRepository);
     }
 
     @Test
-    void findMovieById() {
+    void canFindAllMovies() {
+        underTest.findAllMovies();
+        verify(movieRepository).findAll();
+    }
+
+    @Test
+    void canFindMovieById() {
     }
 }

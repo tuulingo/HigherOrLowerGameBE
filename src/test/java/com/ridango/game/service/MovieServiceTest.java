3467ce59b436ce81bebe89b3ae0fb1eb193bc731
tuulingo <oliver.tuuling@tptlive.ee>
2022-10-24 14:22:40 +0300

    Add explaining comments in testcase / delete unnecessary imports

--- src/test/java/com/ridango/game/service/MovieServiceTest.java
+++ src/test/java/com/ridango/game/service/MovieServiceTest.java
@@ -1,18 +1,13 @@
 package com.ridango.game.service;
 
-import com.ridango.game.model.Movie;
 import com.ridango.game.repository.MovieRepository;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import java.math.BigInteger;
 
@@ -35,16 +30,20 @@ class MovieServiceTest {
 
     @Test
     void canFindAllMovies() {
+        // when
         underTest.findAllMovies();
+        // then
         verify(movieRepository).findAll();
     }
 
     @Test
     void willThrowWhenFindMovieNotFound() {
+        // given
         BigInteger id = BigInteger.valueOf(10);
         given(movieRepository.existsById(id))
                 .willReturn(false);
-
+        // when
+        // then
         assertThatThrownBy(() -> underTest.findMovieById(id))
                 .isInstanceOf(IllegalStateException.class)
                 .hasMessageContaining("movie with id " + id + " does not exist");
@@ -54,11 +53,14 @@ class MovieServiceTest {
 
     @Test
     void canFindMovieById() {
+        // given
         BigInteger id = BigInteger.valueOf(10);
         given(movieRepository.existsById(id)).willReturn(true);
 
+        // when
         underTest.findMovieById(id);
 
+        // then
         verify(movieRepository).findById(id);
     }
 
